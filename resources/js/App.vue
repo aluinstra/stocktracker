25c3d3c562aca935b28e587c45a9998a342feb8e
DESKTOP-EGS74SM\Luin <luin@live.nl>
2022-03-17 16:15:26 +0100

    update: working on nav setting up for building account/dashboard

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,9 +1,34 @@
 <template>
     <div>
         <NavBar />
-        <SideBar />
-        <Footer />
-        <router-view />
+
+        <div class="container-fluid">
+            <div class="row">
+                <SideBar />
+                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 py-4">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item">
+                                <a href="#">Home</a>
+                            </li>
+                            <li
+                                class="breadcrumb-item active"
+                                aria-current="page"
+                            >
+                                Overview
+                            </li>
+                        </ol>
+                    </nav>
+                    <!-- <p>
+                        This is the homepage of a simple admin interface which
+                        is part of a tutorial written on Themesberg
+                    </p>
+                    <h1 class="h2">Dashboard</h1> -->
+                    <router-view />
+                    <Footer />
+                </main>
+            </div>
+        </div>
     </div>
 </template>
 
